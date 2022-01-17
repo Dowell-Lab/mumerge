4d0f0548b4d49adfa24e1db08654c372f3635ca5
jtstanley <jtstan@gmail.com>
2022-01-16 17:37:40 -0700

    Created unittest file. To be developed after modularizing mumerge.py

---
+++ mumerge_unittest.py
@@ -0,0 +1,29 @@
+import unittest
+import blank_lib
+
+class TestLib(unittest.TestCase):
+    
+#    @classmethod
+#    def setUpClass(cls):
+#        print("\n Running class setUp...")
+        
+#    @classmethod
+#    def tearDownClass(cls):
+#        print("\n Running class tearDown...")
+    
+#    def setUp(self):
+#        print("\nRunning setUp...")
+    
+#    def tearDown(self):
+#        print("Running tearDown...")
+        
+    def test_func(self):
+        print("Running test...")
+#        self.assertIsNone()
+#        self.assertEqual()
+#        self.assertAlmostEqual()
+#        self.assertRaises()
+    
+
+if __name__ == "__main__":
+    unittest.main()
