38c9fcb346bb28423e1652b31ec1ce96d4099cc2
jtstanley <jtstan@gmail.com>
2022-01-20 01:05:26 -0700

    Added blank function defs to be filled in later, in unittest.

--- mumerge/test/mumerge_unittest.py
+++ mumerge/test/mumerge_unittest.py
@@ -1,5 +1,5 @@
 import unittest
-from mumerge import mumerge
+from mumerge import mumerge as mm
 
 class TestLib(unittest.TestCase):
     
@@ -24,6 +24,69 @@ class TestLib(unittest.TestCase):
 #        self.assertAlmostEqual()
 #        self.assertRaises()
     
+    def test_normal(self):
+        res = mm.normal()
+        self.assert(res, )
+
+    def test_overlap_check(self):
+        pass
+
+    def test_chromosome_list(self):
+        pass
+    
+    def test_closest_idx(self):
+        pass
+
+    def test_prod(self):
+        pass
+
+    def test_normalizer(self):
+        pass
+
+    def test_bedfile_reader(self):
+        pass
+
+    def test_tfit_dict_initializer(self):
+        pass
+
+    def test_tfit_file_reader(self):
+        pass
+
+    def test_mu_dict_generator(self):
+        pass
+
+    def test_call_remover(self):
+        pass
+
+    def test_prob_list_generator(self):
+        pass
+
+    def test_prob_product(self):
+        pass
+
+    def test_prob_list_formatter(self):
+        pass
+
+    def test_combined_prob_calculator(self):
+        pass
+
+    def test_maxima_loc(self):
+        pass
+
+    def test_mu_sig_extract(self):
+        pass
+
+    def test_mu_ranker(self):
+        pass
+
+    def test_sigma_assigner(self):
+        pass
+
+    def test_collision_resolver(self):
+        pass
+
+    def test_bed_line_formatter(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
