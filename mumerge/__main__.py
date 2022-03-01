1f90a5c88a1ac73157b1f90d199f29f53ed404be
jtstanley <jtstan@gmail.com>
2022-03-01 10:33:39 -0700

    update __main__

--- mumerge/__main__.py
+++ mumerge/__main__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 srcdirectory = Path(__file__).absolute().parent
 sys.path.insert(0, srcdirectory)
 
-print("Running __main__")
+#print("Running __main__")
 
 from mumerge import mumerge
 mumerge.main()
