cd5c5b0daf4225e85be8719ac6ae3f4ad82c5c29
jtstanley <jtstan@gmail.com>
2022-03-10 18:58:16 -0700

    Fixing PyPI script

--- pypi_upload.sh
+++ pypi_upload.sh
@@ -3,5 +3,5 @@ rm -r ./dist/*
 python -m pip install --user --upgrade setuptools wheel
 python setup.py sdist bdist_wheel
 python -m pip install --user --upgrade twine
-python -m twine upload dist/*
+python -m twine upload ./dist/*
 #python -m twine check ./dist/*
